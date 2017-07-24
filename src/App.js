fc13e6f4664ae092b3ed13978447e52edf2ffffa
Yang zhou <you@example.com>
2017-07-24 11:40:06 +0800

    修改首页登陆页

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import WrappedNormalLoginForm from './Login';
 import './App.css';
 class App extends Component {
   render() {
@@ -10,12 +11,11 @@ class App extends Component {
                src={ logo }
                className="App-logo"
                alt="logo" />
-          <h2>Welcome to React12</h2>
+          <h2 className="App-title">欢迎使用家辉培优教学资源管理系统</h2>
+          <p className="App-intro">
+            <WrappedNormalLoginForm/>
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit
-          <code>src / App.js</code> and save to reload.
-        </p>
       </div>);
   }
 }
